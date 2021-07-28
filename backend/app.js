f613d01915dbf7cb7707865c40b8adb34fa7e04c
fnovelli <f.novelli@hotmail.fr>
2021-07-28 16:53:08 +0200

    small adjustments

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,11 @@ const app = express();
 
 //allow app to use API
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');  //allow everyone
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');  //allow everyone
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
   app.use(bodyParser.json());
   app.use('/images', express.static(path.join(__dirname, 'images')));
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,7 +10,7 @@ exports.signup = (req, res, next) => {
           password: hash
       });
       user.save()
-      .then(() => res.status(201).json({message: 'utilisateur créé!'}))
+      .then(() => res.status(201).json({message: 'User created!'}))
       .catch(error => res.status(400).json({error}));
   })
   .catch(error => res.status(500).json({error}));
