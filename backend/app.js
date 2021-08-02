fa6146f853cbea9aab6dbf6713b14e78d25bff2b
fnovelli <f.novelli@hotmail.fr>
2021-08-02 14:32:40 +0200

    fixed auth security typo and CORS adjust

--- backend/app.js
+++ backend/app.js
@@ -19,13 +19,6 @@ mongoose.connect('mongodb+srv://root:' + json + '@cluster0.ywlii.mongodb.net/myF
   );
 
 //allow app to use API
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
   app.use(bodyParser.json());
   app.use(cors())
 
