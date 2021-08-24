4ead4b1a5e5aa6971abefdd71496019006d04a26
fnovelli <f.novelli@hotmail.fr>
2021-08-24 16:19:53 +0200

    added mongoose Sanitize and express ramelimit

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -32,8 +32,6 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(400).json({error}));
   })
   .catch(error => res.status(500).json({error}));
-
-
 };
 
 exports.login = (req, res, next) => {
@@ -46,7 +44,7 @@ exports.login = (req, res, next) => {
       bcrypt.compare(req.body.password, user.password)
       .then(valid => {
           if (!valid) {
-              return res.Status(401).json( { error: 'Incorect password.'});
+              return res.Status(401).json( { error: 'Incorrect password.'});
           }
           res.status(200).json({
               userId: user._id,
